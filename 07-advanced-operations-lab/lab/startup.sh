9e354329c65d8a7970f455f180761b58abc200c0
Mike 'MJ' Johnson <mikejohnsonit@gmail.com>
2019-08-09 09:02:27 -0400

    add advanced 2

--- 07-advanced-operations-lab/lab/startup.sh
+++ 07-advanced-operations-lab/lab/startup.sh
@@ -6,14 +6,7 @@ mkdir -p /root/resources
 cp -r /root/newresources/* /root/resources
 rm -rf /root/newresources
 kubectl delete all --all
+minikube start
 minikube addons enable heapster
 minikube addons enable metrics-server
 minikube addons enable dashboard
-
-
-export NODE01=$(tail -n 1 /etc/hosts | awk '{print $1}')
-echo "$NODE01 default\n\
-$NODE01 green.algo\n\
-$NODE01 blue.algo\n\
-$NODE01 weighted.algo" >> /etc/hosts
-chmod +x /root/resources/weighted-test.sh
